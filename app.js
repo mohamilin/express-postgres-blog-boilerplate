a8ef7d4fde6090595caa12b8608daec002b593bf
Moh Amilin <amilinmoh99@gmail.com>
2021-04-18 20:57:04 +0700

    add : konfigurasu koneksi database dg express

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
+require('dotenv').config()
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { Sequelize } = require('sequelize');
+const {development} = require('./config/config')
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const sequelize = new Sequelize(development.database, development.username, development.password, {
+  host: development.host,
+  dialect: development.dialect
+});
+const app = express();
+try {
+  sequelize.authenticate()
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
