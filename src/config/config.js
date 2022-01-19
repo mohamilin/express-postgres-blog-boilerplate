d2cb60ba173d4b9bb5cc6d01c6ad6603b718fe0e
Moh Amilin <amilinmoh99@gmail.com>
2022-01-19 17:32:31 +0700

    update configuration database in config

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,26 @@
-require('dotenv').config()
-module.exports = {
-    development: {
-      username : process.env.DB_USERNAME,
-      password : process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      host: "127.0.0.1",
-      dialect: "postgres"
-    } 
-}
+require('dotenv').config();
+
+const environment = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+};
+module.exports = environment;
