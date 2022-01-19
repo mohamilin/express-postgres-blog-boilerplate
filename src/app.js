7942f58b071f94441e31432f712d9f395c5976b7
Moh Amilin <amilinmoh99@gmail.com>
2022-01-19 17:34:22 +0700

    delete code for connect to DB

--- src/app.js
+++ src/app.js
@@ -3,28 +3,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const { Sequelize } = require('sequelize');
-const {development} = require('./config/config')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-// connect to database
-const sequelize = new Sequelize(development.database, development.username, development.password, {
-  host: development.host,
-  dialect: development.dialect
-});
-
-sequelize.sync()
-  .then(() => {
-  console.log({status: 'success', message: 'DB connection sucessful.'});
-  }).catch(err => {
-  console.log({status: 'failed', message: 'Unable to connect to the database', error:  err.message});
-  })
-
 
 const app = express();
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,12 +23,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
