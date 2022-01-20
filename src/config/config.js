0220d461e9da3fdb5f9a1c08f5df9c15224d5eef
Moh Amilin <amilinmoh99@gmail.com>
2022-01-20 14:27:33 +0700

    update : env for dev and test

--- src/config/config.js
+++ src/config/config.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 const environment = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME_DEV,
+    password: process.env.DB_PASSWORD_DEV,
+    database: process.env.DB_DATABASE_DEV,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_DATABASe_TEST,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
