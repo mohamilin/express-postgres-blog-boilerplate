004e97d4568e2f194a944b6b5f71939b56a0cae6
Moh Amilin <amilinmoh99@gmail.com>
2022-01-20 14:27:52 +0700

    delete env for dev

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,5 +1,5 @@
 const winston = require('winston');
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV
 const config = require(`${__dirname}/../config/config.js`)[env];
 
 const logger = winston.createLogger({
