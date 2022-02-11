5cd4ce9cb0250cfa8e3b9aac954e9d1c4abe27c6
Moh Amilin <amilinmoh99@gmail.com>
2022-02-11 21:48:26 +0700

    refactor routes endpoint and use cors

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const cors = require('cors');
 
+const apiRouter = require('./routes/api');
 
 const app = express();
 
@@ -13,14 +13,14 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
