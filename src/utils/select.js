487cbeb28a8d27fc866e5c507639b8c49f0c1422
Moh Amilin <amilinmoh99@gmail.com>
2022-02-12 18:50:30 +0700

    add : select as pick or same lodash

---
+++ src/utils/select.js
@@ -0,0 +1,12 @@
+const select = (object, keys) => {
+  return keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      // eslint-disable-next-line no-param-reassign
+      obj[key] = object[key];
+    }
+
+    return obj;
+  }, {});
+};
+
+module.exports = select;
