17996d430c6f48b761e2e42dd301cdbc54b5903c
Moh Amilin <amilinmoh99@gmail.com>
2022-02-12 13:40:50 +0700

    update : completed logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,18 +1,24 @@
 const winston = require('winston');
 const setting = require('./settings');
 
+const ennumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
 const logger = winston.createLogger({
   level: setting.env === 'development' ? 'debug' : 'info',
+  format: winston.format.combine(
+    ennumerateErrorFormat(),
+    setting.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level} : ${message}`)
+  ),
   transports: [
     new winston.transports.Console({
-      level: 'info',
       stderrLevels: ['error'],
-      format: winston.format.combine(
-        setting.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
-        winston.format.splat(),
-        winston.format.printf(({ level, message }) => `${level} : ${message}`)
-      ),
-      handleExceptions: true,
     }),
   ],
 });
