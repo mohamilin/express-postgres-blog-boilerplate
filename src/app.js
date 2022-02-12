8bc2e1e910c766e905229bb236521a24a701ca2b
Moh Amilin <amilinmoh99@gmail.com>
2022-02-12 13:44:43 +0700

    update : use errorHandler, morgan, cors

--- src/app.js
+++ src/app.js
@@ -2,19 +2,33 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const cors = require('cors');
 
+const { env } = require('./config/settings');
+
+const morgan = require('./middlewares/morgan');
+const { errorConverter, errorException } = require('./middlewares/errorHandler');
+
 const apiRouter = require('./routes/api');
+const AppError = require('./utils/AppError');
+const httpStatus = require('http-status');
 
 const app = express();
+
+if (env !== 'test') {
+  app.use(morgan.successHandler);
+  app.use(morgan.errorHandler);
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// enable cors
 app.use(cors());
-app.use(logger('dev'));
+app.options('*', cors());
+
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -28,14 +42,13 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((req, res, next) => {
+  next(new AppError(httpStatus.NOT_FOUND, 'Not found'));
 });
+
+// convert error to AppError, if needed
+app.use(errorConverter);
+// handle error
+app.use(errorException);
 
 module.exports = app;
