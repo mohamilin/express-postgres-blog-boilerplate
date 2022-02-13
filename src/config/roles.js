8c0e542d87c550794ec87538a3356be4e088d21e
Moh Amilin <amilinmoh99@gmail.com>
2022-02-13 22:42:47 +0700

    update roles

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,7 +1,22 @@
+/**
+ * TYPES OF RULE
+ * 1 = superAdmin
+ * 2 = admin
+ * 3 = user
+ */
+
+/**
+ * Spesific Task 
+ * Role superAdmin := dapat melakukan 3 tugas khusus ; 'managerUsers', 'getUsers', 'manageCategory'
+ * Role admin := dapat melakukan 2 tugas khusu ; 'getUsers', 'manageCategory'
+ * Role user := tidak memiliki tugas khusus.
+ * 
+ */
+
 const listRoles = {
-  superAdmin: ['managerUsers', 'getUsers'],
-  admin: ['getUsers'],
-  user: [],
+  1: ['managerUsers', 'getUsers', 'manageCategory'],
+  2: ['getUsers', 'manageCategory'],
+  3: [],
 };
 
 const roles = Object.keys(listRoles);
