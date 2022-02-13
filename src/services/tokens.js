482633c1c2ad973e52ba523d544b727034d47503
Moh Amilin <amilinmoh99@gmail.com>
2022-02-13 11:24:08 +0700

    add : token service

---
+++ src/services/tokens.js
@@ -0,0 +1,66 @@
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+const config = require('../config/settings');
+const { tokenTypes } = require('../config/tokens');
+const Model = require('../models');
+const { tokens } = Model.sequelize.models;
+
+/**
+ *
+ */
+const generateToken = (userId, expires, type, secret = config.jwt.secret) => {
+  const payload = {
+    sub: userId,
+    iat: moment().unix(),
+    exp: expires.unix(),
+    type,
+  };
+  return jwt.sign(payload, secret);
+};
+
+/**
+ *
+ */
+
+const saveToken = async (token, userId, expires, type, blacklisted = false) => {
+  const tokenDoc = await tokens.create({
+    token,
+    user: userId,
+    expires: expires.toDate(),
+    type,
+    blacklisted,
+  });
+
+  return tokenDoc;
+};
+
+/**
+ * 
+ */
+
+const generateTokenAuth = async (user) => {
+  const accessTokenExp = moment().add(config.jwt.accessExpirationMinutes, 'minutes');
+  const accessToken = generateToken(user.id, accessTokenExp, tokenTypes.ACCESS_TOKEN);
+
+  const refreshTokenExp = moment().add(config.jwt.refreshExpirationDays, 'days');
+  const refreshToken = generateToken(user.id, refreshTokenExp, tokenTypes.REFRESH_TOKEN);
+
+  await saveToken(refreshToken, user.id, refreshTokenExp, tokenTypes.REFRESH_TOKEN);
+
+  return {
+    access: {
+      token: accessToken,
+      expires: accessTokenExp.toDate(),
+    },
+    refresh: {
+      token: refreshToken,
+      expires: refreshTokenExp.toDate(),
+    },
+  };
+};
+
+module.exports = {
+  generateToken,
+  saveToken,
+  generateTokenAuth,
+};
