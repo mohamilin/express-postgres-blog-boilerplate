ebbb577d02d7862b47a98a6b9a749126f956fcf8
Moh Amilin <amilinmoh99@gmail.com>
2022-02-13 09:32:57 +0700

    update : add jwt object

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,9 +1,14 @@
-const path = require('path')
+const path = require('path');
 require('dotenv').config({
-  path: path.join(__dirname, '../../.env')
+  path: path.join(__dirname, '../../.env'),
 });
 
 module.exports = {
   env: process.env.NODE_ENV,
-  port: process.env.PORT
-}
+  port: process.env.PORT,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    accessExpirationMinutes: process.env.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: process.env.JWT_REFRESH_EXPIRATION_DAYS,
+  },
+};
