cef7ab67501cfdc0de31aa77ea701f67ae113be4
Moh Amilin <amilinmoh99@gmail.com>
2022-02-13 09:34:01 +0700

    add : authRateLimiter

---
+++ src/middlewares/authRateLimiter.js
@@ -0,0 +1,10 @@
+const rateLimit = require('express-rate-limit');
+
+const authRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20,
+  skipSuccessfulRequests: true,
+});
+
+
+module.exports = authRateLimiter;
