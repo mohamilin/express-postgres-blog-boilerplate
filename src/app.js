ada1908798bee99abd07c7e1b30dd051f893eda8
Moh Amilin <amilinmoh99@gmail.com>
2022-02-13 09:34:52 +0700

    update : use httpStatus, passport, jwtStrategy, authRateLimiter

--- src/app.js
+++ src/app.js
@@ -3,14 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const httpStatus = require('http-status');
+const passport = require('passport');
 
+// config
 const { env } = require('./config/settings');
+const jwtStrategy = require('./config/passport');
 
+// middleware
 const morgan = require('./middlewares/morgan');
 const { errorConverter, errorException } = require('./middlewares/errorHandler');
-const AppError = require('./utils/AppError');
-const httpStatus = require('http-status');
+const authRateLimiter = require('./middlewares/authRateLimiter');
 
+// utils
+const AppError = require('./utils/AppError');
+
+// routes
 const apiRouter = require('./routes/api');
 
 const app = express();
@@ -28,11 +36,19 @@ app.set('view engine', 'ejs');
 app.use(cors());
 app.options('*', cors());
 
-// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+
+// jwt | passport
+app.use(passport.initialize());
+passport.use('jwt', jwtStrategy);
+
+// limit repeated failed requests to auth endpoints
+if (env === 'production') {
+  app.use('/api/v1/auth', authRateLimiter);
+}
 
 app.use('/api/v1', apiRouter);
 
@@ -46,7 +62,7 @@ app.use((req, res, next) => {
   next(new AppError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
-// convert error to AppError, if needed
+// boolean needed, convert error to AppError, 
 app.use(errorConverter);
 // handle error
 app.use(errorException);
