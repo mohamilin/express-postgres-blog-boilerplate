496188d71f052f44eb0bea1f897ab19ce63cddcc
Moh Amilin <amilinmoh99@gmail.com>
2022-02-15 11:45:57 +0700

    update : add handle err if route not found

--- src/app.js
+++ src/app.js
@@ -64,6 +64,15 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+   });
+
 // error handler
 app.use((req, res, next) => {
   next(new AppError(httpStatus.NOT_FOUND, 'Not found'));
