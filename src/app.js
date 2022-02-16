4c32d6ddbc7762e359b579a9eddaaa6656f7e822
Moh Amilin <amilinmoh99@gmail.com>
2022-02-16 13:11:27 +0700

    delete handle error

--- src/app.js
+++ src/app.js
@@ -64,14 +64,6 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-   });
 
 // error handler
 app.use((req, res, next) => {
