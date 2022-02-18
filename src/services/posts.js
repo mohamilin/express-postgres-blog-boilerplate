b99e3e22e170a12d2415bba48c694939e16255ff
Moh Amilin <amilinmoh99@gmail.com>
2022-02-18 09:40:46 +0700

    updated : services posts

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,36 +1,99 @@
-const httpStatus = require('http-status');
-const { categoryService } = require('.');
-const Model = require('../models');
-const AppError = require('../utils/AppError');
-const { posts, categories, posts_categories } = Model.sequelize.models;
+const { getCategoryById } = require('./categories');
+const { addPostsTags } = require('./tags');
+const Model = require('../database/models');
+
+const { posts, users, categories, posts_categories, tags, comments } = Model.sequelize.models;
 
 const getPosts = async () => {
-    const post = await posts.findAll()
-    return post
-}
+  const post = await posts.findAll({
+    include: [
+      {
+        model: categories,
+        as: 'categories',
+        through: {
+          attributes: [],
+        },
+      },
+      {
+        model: users,
+        as: 'users',
+        attributes: ['id', 'userName'],
+      },
+    ],
+  });
+  return post;
+};
 
 const addPosts = async (user, payload) => {
-    const { id } = user
-    const { categoryId, title, slug, short_desc, content } = payload
-    await categoryService.getCategoryById(categoryId)
-    const post = await posts.create({ categoryId, userId: id, title, slug, short_desc, content })
-    await posts_categories.create({
-        postId: post.id,
-        categoryId: categoryId
-    })
-    return post
-}
+  const { id } = user;
+  const { categoryId, title, slug, short_desc, content } = payload;
+  const post = await posts.create({ categoryId, userId: id, title, slug, short_desc, content });
+  await posts_categories.create({
+    postId: post.id,
+    categoryId,
+  });
+  return post;
+};
 
-const getPostsById = async(id) => {
-    const post = await posts.findOne({where: {id:id}})
-    if(!post) {
-        throw new AppError(httpStatus.NOT_FOUND, `id ${id} post tidak ditemukan`)
-    }
+const getPostsById = async (id) => {
+  const post = await posts.findOne({
+    where: { id },
+    include: [
+      {
+        model: categories,
+        as: 'categories',
+        through: {
+          attributes: [],
+        },
+      },
+      {
+        model: users,
+        as: 'users',
+        attributes: ['id', 'userName'],
+      },
+      {
+        model: tags,
+        as: 'tags',
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  });
+  return post;
+};
 
-    return post
-}
+const getPostsBySlug = async (slug) => {
+  const post = await posts.findOne({
+    where: { slug:slug },
+    include: [ "comments",
+      {
+        model: categories,
+        as: 'categories',
+        through: {
+          attributes: [],
+        },
+      },
+      {
+        model: users,
+        as: 'users',
+        attributes: ['id', 'userName'],
+      },
+      {
+        model: tags,
+        as: 'tags',
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  });
+  return post;
+};
+
 module.exports = {
-    getPosts,
-    addPosts,
-    getPostsById
-}
+  getPosts,
+  addPosts,
+  getPostsById,
+  getPostsBySlug
+};
