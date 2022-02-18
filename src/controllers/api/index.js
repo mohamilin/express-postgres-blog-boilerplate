a39b07edce2caac5b374520ed56a8ed20382b1c0
Moh Amilin <amilinmoh99@gmail.com>
2022-02-18 09:31:45 +0700

    update : tag and comment controller

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,3 +1,5 @@
 module.exports.userControllers = require('./users');
 module.exports.categoryController = require('./categories');
 module.exports.postControllers = require('./posts');
+module.exports.tagControllers = require('./tags');
+module.exports.commentControllers = require('./comments');
