650726ca050d6345b8d91b6bf4b132808e0a9c18
Moh Amilin <amilinmoh99@gmail.com>
2022-02-18 09:41:07 +0700

    update : add tagService & commentService

--- src/services/index.js
+++ src/services/index.js
@@ -2,3 +2,5 @@ module.exports.userService = require('./users');
 module.exports.tokenService = require('./tokens');
 module.exports.categoryService = require('./categories');
 module.exports.postService = require('./posts');
+module.exports.tagService = require('./tags');
+module.exports.commentService = require('./comments');
