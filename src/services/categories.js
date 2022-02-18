86aaccbbf3bb875fa507c305f94816cd44b0082a
Moh Amilin <amilinmoh99@gmail.com>
2022-02-18 09:39:00 +0700

    update : services categories for include

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,5 +1,6 @@
-const Model = require('../models');
-const { categories } = Model.sequelize.models;
+const Model = require('../database/models');
+
+const { categories, posts, users } = Model.sequelize.models;
 
 const addCategory = async (user, payload) => {
   const { id } = user;
@@ -9,17 +10,50 @@ const addCategory = async (user, payload) => {
 };
 
 const getCategory = async () => {
-  const category = await categories.findAll();
+  const category = await categories.findAll({
+    include: [
+      {
+        model: posts,
+        as: 'posts',
+        through: {
+          attributes: [],
+        },
+      },
+      {
+        model: users,
+        as: 'users',
+        attributes: ['id', 'fullName'],
+      },
+    ],
+  });
   return category;
 };
 
 const getCategoryById = async (id) => {
-  const category = await categories.findOne({where: {id: id}});
+  const category = await categories.findOne({
+    where: {
+      id,
+    },
+    include: [
+      {
+        model: posts,
+        as: 'posts',
+        through: {
+          attributes: [],
+        },
+      },
+      {
+        model: users,
+        as: 'users',
+        attributes: ['id', 'fullName'],
+      },
+    ],
+  });
   return category;
 };
 
 const getCategoryBySlug = async (slug) => {
-  const category = await categories.findOne({ where: { slug: slug } });
+  const category = await categories.findOne({ where: { slug } });
   return category;
 };
 
