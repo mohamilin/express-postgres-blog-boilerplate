e26b7b9fac19c5a19fc612ef968a6f264b6671ed
Moh Amilin <amilinmoh99@gmail.com>
2022-02-18 09:26:03 +0700

    deleted created & updated nou use

--- src/database/migrations/20210422134107-create-comments.js
+++ src/database/migrations/20210422134107-create-comments.js
@@ -22,14 +22,8 @@ module.exports = {
       content: {
         type: Sequelize.TEXT,
       },
-      createdAt: {
-        type: Sequelize.INTEGER,
-      },
       image: {
         type: Sequelize.STRING,
-      },
-      updatedAt: {
-        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
