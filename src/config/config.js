85759c80333dd65a213fe708653d9f27be68e0fd
mohamilin <amilinmoh99@gmail.com>
2022-04-06 14:29:58 +0700

    change env for production

--- src/config/config.js
+++ src/config/config.js
@@ -16,11 +16,14 @@ const environment = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
 module.exports = environment;
