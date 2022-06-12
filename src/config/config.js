0c31b81e2d729eca2c94449ca7408948b82eb72f
amilin <moh.amilin@moladin.com>
2022-06-12 10:44:49 +0700

    change DB_DIALECT for dev and test

--- src/config/config.js
+++ src/config/config.js
@@ -6,14 +6,14 @@ const environment = {
     password: process.env.DB_PASSWORD_DEV,
     database: process.env.DB_DATABASE_DEV,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: process.env.DB_DIALECT_DEV,
   },
   test: {
     username: process.env.DB_USERNAME_TEST,
     password: process.env.DB_PASSWORD_TEST,
     database: process.env.DB_DATABASE_TEST,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: process.env.DB_DIALECT_TEST,
   },
   production: {
     use_env_variable: 'DATABASE_URL',
